8d52ebd4500ac82d9661e958fc86f9ff097ebfff
Baraka Ashery <brkmagdalena@gmail.com>
2019-07-12 18:27:13 +0300

    Add files via upload

---
+++ sidenav.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("side-nav").style.width = "90vw";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("side-nav").style.width = "0";
+} 
