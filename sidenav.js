0b623ee0f7b5d9bd95b0b4515ca988faa359e5b0
Baraka Ashery <brkmagdalena@gmail.com>
2019-07-14 12:49:02 +0300

    Delete sidenav.js

--- sidenav.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("side-nav").style.width = "90vw";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("side-nav").style.width = "0";
-} 
